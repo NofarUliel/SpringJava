930f4f52709e556984044e15e394b53df0244d74
Nofar Uliel <nofaruliel@gmail.com>
2021-02-28 12:56:18 +0200

    constructor injection

--- src/com/luv2code/springdemo/BaseballCoach.java
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -2,8 +2,22 @@ package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach {
 
+	//define a private field for dependency
+	private FortuneService fortuneService;
+	
+	
+	//define a constructor for dependency injection
+	public BaseballCoach(FortuneService theFortuneService) {
+		this.fortuneService=theFortuneService;
+	}
 	@Override
-	public String getDailyWorkput() {
+	public String getDailyWorkout() {
 		return "spend 30 minutes on practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		//use my fortuneService to get a fortune
+		return this.fortuneService.getFortune();
 	}
 }
--- src/com/luv2code/springdemo/Coach.java
+++ src/com/luv2code/springdemo/Coach.java
@@ -2,7 +2,8 @@ package com.luv2code.springdemo;
 
 public interface Coach {
 	
-	public String  getDailyWorkput();
+	public String  getDailyWorkout();
+	public String getDailyFortune();
 
 	
 	
